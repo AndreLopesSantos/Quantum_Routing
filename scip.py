2059b03c04bbe998544ee939df7fea8b9d2dd948
André Lopes dos Santos <up200505634@edu.fe.up.pt>
2021-04-23 16:09:00 +0100

    SCIP done (only cleanup)

--- scip.py
+++ scip.py
@@ -4,25 +4,72 @@ import re
 from datetime import datetime
 
 
-def write_lp_file(G,A_value,B_value):
+def write_lp_file(G,B_value):
     num_nodes = G.number_of_nodes()
     num_variables = num_nodes **2
+
+    #Writing the objective function
+    objective=" obj: "
+    for i,j in G.edges:
+        weight = G.get_edge_data(*(i,j))['weight']
+        weight *= 2
+        for b in range(num_nodes):
+            objective +=  " [ " + str(weight) + " x" + str(i) + "_" + str(b+1) + " * " + "x" + str(j) + "_" + str(b+2) + " ]/2 + "
+            if isinstance(G, nx.classes.digraph.DiGraph) == False:
+                objective += " [ " + str(weight) + " x" + str(j) + "_" + str(b+1) + " * " + "x" + str(i) + "_" + str(b+2) + " ]/2 + "
+
+            if b == num_nodes -2:
+                break
+        
+    objective = objective[:-3]
+    objective += "\n"
+
+    #Writing constraints
+    constraint = 1
+    constraint_str = ""
+    for a in range(num_nodes):
+        constraint_str += " c" + str(constraint) + ": "
+        for c in range(num_nodes):
+            constraint_str +=  "x" + str(a) + "_" + str(c+1) + " + "
+
+        constraint_str = constraint_str[:-3]
+        constraint_str += " = 1\n"
+        constraint += 1
+
+    for n in range(num_nodes):
+        constraint_str += " c" + str(constraint) + ": "
+        for m in range(num_nodes):
+            constraint_str +=  "x" + str(m) + "_" + str(n+1) + " + "
+
+        constraint_str = constraint_str[:-3]
+        constraint_str += " = 1\n"
+        constraint += 1
+
+
+    constraint_str += " c" + str(constraint) + ": "  + "x0_1 = 1\n"
+    constraint += 1
+    constraint_str += " c" + str(constraint) + ": "  + "x" + str(num_nodes-1) + "_" + str(num_nodes) + " = 1\n"
+
+    #Binary Variables
+    binstr = ""
+    for k in range(num_nodes):
+        for h in range(num_nodes):
+            binstr += " x" + str(k) + "_" + str(h+1) + "\n"
+
+
+
+
     #now = datetime.now()
     #date_string = now.strftime("%d-%m-%Y_%H-%M-%S")
     #filename = "nodes_"+str(num_nodes)+"_date_"+date_string+".lp"
     filename = "simple.lp"
     f = open(filename, "w")
     f.write("Minimize\n")
-    f.write(" obj: x1 + 2 x2 + 3 x3 + x4\n")
+    f.write(objective)
     f.write("Subject To\n")
-    f.write(" c1: - x1 + x2 + x3 + 10 x4 <= 20\n") #dependendo do número de nós será algo do tipo x11 + x12 + x13 + x14 = 1 e x21 + x22 + x23 + x24 = 1 , etc.
-    f.write(" c2: x1 - 3 x2 + x3 <= 30\n")
-    f.write(" c3: x2 - 3.5 x4 = 0\n")
-    f.write("Bounds\n")
-    f.write(" 0 <= x1 <= 40\n") #bounds para todos serão 0<= x1 <= 1
-    f.write(" 2 <= x4 <= 3\n")
-    f.write("General\n")
-    f.write(" x4\n")
+    f.write(constraint_str) #dependendo do número de nós será algo do tipo x11 + x12 + x13 + x14 = 1 e x21 + x22 + x23 + x24 = 1 , etc.)
+    f.write("Binary\n")
+    f.write(binstr)
     f.write("End")
     f.close()
 
@@ -31,7 +78,8 @@ def read_sol_file(G):
     print("READING SOLUTION FILE")
     filename = "simple.sol"
     f = open(filename, "r")
-    resultados = [0] * (G.number_of_nodes()+1)
+    number_of_nodes = G.number_of_nodes()
+    resultados = [0] * ((number_of_nodes**2) + 1)
     num_linha = 0
     for linha in f:
         num_linha += 1
@@ -41,8 +89,10 @@ def read_sol_file(G):
             print("Objective value: " ,objective_value)
         if num_linha > 2:
             x_value = re.findall("\d+",linha)
-            print("x_value: " ,x_value)
-            resultados[int(x_value[0])] = int(x_value[1])
+            if len(x_value) > 2:
+                print("x_value: " ,x_value)
+                local_x = int(x_value[0]) * number_of_nodes + int(x_value[1])
+                resultados[local_x] = int(x_value[2])
 
     print("Resultados:")
     print(resultados)
@@ -58,9 +108,9 @@ def read_sol_file(G):
     #print(objective_value)
 
 G = nx.Graph()
-G.add_weighted_edges_from({(0,1,7.0),(0,2,9.0),(1,2,10.0),(1,3, 15.0),(2,3, 11.0),(3,4, 6.0),(4,5, 9.0),(5,0,14.0),(2,5, 2.0)})
-#write_lp_file(G)
-print("Number of nodes in G: ",G.number_of_nodes() **2 )
-
+#G.add_weighted_edges_from({(0,1,7.0),(0,2,9.0),(1,2,10.0),(1,3, 15.0),(2,3, 11.0),(3,4, 6.0),(4,5, 9.0),(5,0,14.0),(2,5, 2.0),(0,3,3.0),(0,4, 5.0), (1,4,2.0), (1,5,1.0), (2,4,2.0),(3,5,6.0)})
+G.add_weighted_edges_from({(0,1,20.0),(0,2,19.0),(1,2,2.0),(1,3, 2.0),(2,3, 15.0),(3,4, 16.0),(4,5, 19.0),(5,0,14.0),(2,5, 12.0),(0,3,13.0),(0,4, 2.0), (1,4,12.0), (1,5,11.0), (2,4,2.0),(3,5,2.0)})
+write_lp_file(G,1)
 os.system('cmd /c "scip -c "read simple.lp optimize display solution write solution simple.sol quit""')
 read_sol_file(G)
+
--- simple.sol
+++ simple.sol
@@ -1,4 +1,9 @@
 solution status: optimal solution found
-objective value:                                    2
-x4                                                  1 	(obj:1)
-x1                                                  1 	(obj:1)
+objective value:                                   10
+x0_1                                                1 	(obj:0)
+x1_4                                                1 	(obj:0)
+x2_3                                                1 	(obj:0)
+x3_5                                                1 	(obj:0)
+x4_2                                                1 	(obj:0)
+x5_6                                                1 	(obj:0)
+quadobjvar                                         10 	(obj:1)
