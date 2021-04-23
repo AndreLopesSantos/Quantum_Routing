7548b61d8719f468a278daf0e126502b6fe649eb
André Lopes dos Santos <up200505634@edu.fe.up.pt>
2021-04-23 16:30:45 +0100

    Starting Dwave implementation (WIP)

--- traveling_salesman.py
+++ traveling_salesman.py
@@ -9,11 +9,11 @@ import numpy as np
 G = nx.Graph()
 #G.add_weighted_edges_from({(0, 1, 1), (0, 2, 2), (0, 3, 3), (1, 2, 4),(1, 3, 5), (2, 3, 6)})
 #G.add_weighted_edges_from({(0, 1, .5), (0, 2, .1), (0, 3, .1), (1, 2, .1),(1, 3, .1), (2, 3, .5)})
-G.add_weighted_edges_from({(0,1,7.0),(0,2,9.0),(1,2,10.0),(1,3, 15.0),(2,3, 11.0),(3,4, 6.0),(4,5, 9.0),(5,0,14.0),(2,5, 2.0)}) #complete
+#G.add_weighted_edges_from({(0,1,7.0),(0,2,9.0),(1,2,10.0),(1,3, 15.0),(2,3, 11.0),(3,4, 6.0),(4,5, 9.0),(5,0,14.0),(2,5, 2.0)}) #complete
 #lista = dnx.traveling_salesperson_qubo(G)
+G.add_weighted_edges_from({(0,1,7.0),(0,2,9.0),(1,2,10.0),(1,3, 15.0),(2,3, 11.0),(3,4, 6.0),(4,5, 9.0),(5,0,14.0),(2,5, 2.0),(0,3,3.0),(0,4, 5.0), (1,4,2.0), (1,5,1.0), (2,4,2.0),(3,5,6.0)})
 
-
-def traveling_salesman(G, is_directed_graph = False):
+def traveling_salesman(G):
 
     Q = defaultdict(int)
 
@@ -21,7 +21,10 @@ def traveling_salesman(G, is_directed_graph = False):
     num_nodes = G.number_of_nodes()
     num_edges = G.number_of_edges()
 
-    is_directed_graph = False
+    if isinstance(G, nx.classes.digraph.DiGraph):
+        is_directed_graph = True
+    else:
+        is_directed_graph = False
 
     max_weight = 0
 
