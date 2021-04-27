e081afa5ec0d95d9fa097de48675485c21dc8de3
André Lopes dos Santos <up200505634@fe.up.pt>
2021-04-27 20:25:54 +0100

    Dwave parsing (WIP)

--- traveling_salesman.py
+++ traveling_salesman.py
@@ -7,12 +7,9 @@ import sys
 import numpy as np
 
 G = nx.Graph()
-#G.add_weighted_edges_from({(0, 1, 1), (0, 2, 2), (0, 3, 3), (1, 2, 4),(1, 3, 5), (2, 3, 6)})
-#G.add_weighted_edges_from({(0, 1, .5), (0, 2, .1), (0, 3, .1), (1, 2, .1),(1, 3, .1), (2, 3, .5)})
-#G.add_weighted_edges_from({(0,1,7.0),(0,2,9.0),(1,2,10.0),(1,3, 15.0),(2,3, 11.0),(3,4, 6.0),(4,5, 9.0),(5,0,14.0),(2,5, 2.0)}) #complete
 #lista = dnx.traveling_salesperson_qubo(G)
-G.add_weighted_edges_from({(0,1,7.0),(0,2,9.0),(1,2,10.0),(1,3, 15.0),(2,3, 11.0),(3,4, 6.0),(4,5, 9.0),(5,0,14.0),(2,5, 2.0),(0,3,3.0),(0,4, 5.0), (1,4,2.0), (1,5,1.0), (2,4,2.0),(3,5,6.0)})
-
+#G.add_weighted_edges_from({(0,1,7.0),(0,2,9.0),(1,2,10.0),(1,3, 15.0),(2,3, 11.0),(3,4, 6.0),(4,5, 9.0),(5,0,14.0),(2,5, 2.0),(0,3,3.0),(0,4, 5.0), (1,4,2.0), (1,5,1.0), (2,4,2.0),(3,5,6.0)})
+G.add_weighted_edges_from({(0,1,20.0),(0,2,19.0),(1,2,2.0),(1,3, 2.0),(2,3, 15.0),(3,4, 16.0),(4,5, 19.0),(5,0,14.0),(2,5, 12.0),(0,3,13.0),(0,4, 2.0), (1,4,12.0), (1,5,11.0), (2,4,2.0),(3,5,2.0)})
 def traveling_salesman(G):
 
     Q = defaultdict(int)
@@ -93,7 +90,12 @@ def traveling_salesman(G):
         testarray[alp]=Q[alp]
 
     print(testarray)
-    np.set_printoptions(threshold = False)
+
+    filename = "QuboMatrix.txt"
+    f = open(filename, "w")
+    for linha in testarray:
+        f.write(str(linha))
+
     '''
     lista = nx.to_dict_of_lists(G)
     lista2 = nx.to_edgelist(G)
@@ -131,12 +133,12 @@ def traveling_salesman(G):
         Q[(j,j)] += 1 #xj
         Q[(i,j)] += -2 # -2xixj
 
-
+'''
     sampler = EmbeddingComposite(DWaveSampler())
     sampleset = sampler.sample_qubo(Q, num_reads=10, chain_strength=10)
 
     print(sampleset)
-    '''
+    np.set_printoptions(threshold = False)
 
 
 traveling_salesman(G)
